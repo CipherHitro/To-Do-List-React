6942a42b9c0e90998a16aa3b434bab1a124e7276
CipherHitro <rrohit2911@gmail.com>
2025-05-29 00:31:06 +0530

    css changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -192,7 +192,7 @@ function App() {
             <input
               type="submit"
               value="Save"
-              // disabled={todo.length <= 2}
+              disabled={todo.length <= 2}
               className="cursor-pointer focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2 mb-2 disabled:bg-slate-600 dark:bg-purple-600 dark:hover:bg-purple-900 dark:focus:ring-purple-900"
             />
           </form>
